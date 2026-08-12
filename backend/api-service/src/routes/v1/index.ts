4802211cf52dc17c9ce09e13377e410888c8cb75
aryanracha <aryanracha@gmail.com>
2026-08-13 04:09:20 +0530

    test: if this fails githubRouter is the issue

--- backend/api-service/src/routes/v1/index.ts
+++ backend/api-service/src/routes/v1/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from "hono";
 import authRouter from "../../modules/auth/auth.routes";
-// import githubRouter from "../../modules/github/github.routes";
+import githubRouter from "../../modules/github/github.routes";
 
 const v1Router = new Hono();
 
 v1Router.route("/auth", authRouter);
-// v1Router.route("/github", githubRouter);
+v1Router.route("/github", githubRouter);
 
 export default v1Router;
